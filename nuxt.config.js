b2ca191dab51daf0658065a3e0646413ce4f16ab
Victor Borràs <vborrasc@gmail.com>
2020-10-03 00:15:48 +0200

    feat: change page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'victorborras.dev',
+    title: 'Victor Borràs Castell | victorborras.dev',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
