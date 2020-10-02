8797d5a48eb7f14dbe14fd486b848e366e8fbf3c
Victor Borràs <vborrasc@gmail.com>
2020-10-03 00:33:46 +0200

    chore: add stuff to make lighthouse happy

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,19 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
+    htmlAttrs: {
+      lang: 'en',
+    },
     title: 'Victor Borràs Castell | victorborras.dev',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          "Personal site of Victor Borràs Castell. Someday this will be a blog, now it's just a short introduction.",
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
