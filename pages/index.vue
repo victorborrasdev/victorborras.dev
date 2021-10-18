970ae6240591249fb00a07f279d7aa42e854d8fb
Victor Borràs <vborrasc@gmail.com>
2021-10-18 21:49:10 +0200

    set up eslint

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,7 @@
-<template>
-  <h1>Automatiza las partes aburridas con Hygen</h1>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus dignissim mauris sit amet pretium. Sed
-    convallis massa nisi, eget hendrerit felis euismod a. Proin in dui urna. Suspendisse ut urna nec lacus interdum
-    vestibulum. Maecenas mollis consectetur velit, quis fringilla orci eleifend et. Nunc in augue dapibus, finibus enim
-    et, blandit metus. Curabitur vestibulum sem quis ex vulputate feugiat. Duis iaculis tellus sit amet dignissim
-    ullamcorper. Praesent lectus leo, luctus quis velit in, ultrices dignissim nisi. Praesent tempus quis ante sed
-    tempus. Nulla a leo orci. In congue ex eu consequat dignissim.
-  </p>
-  <p>
-    Phasellus placerat enim at tempus aliquet. Aliquam metus odio, rutrum id massa at, ullamcorper tincidunt nulla.
-    Nulla tempus turpis orci, sit amet rutrum eros consectetur venenatis. Ut vitae neque ac dolor tincidunt volutpat vel
-    et lectus. Etiam ac sodales arcu, vel hendrerit felis. Phasellus tincidunt ultrices dictum. Morbi aliquam mauris
-    augue, vitae luctus nulla facilisis non. Aliquam ut quam dapibus, efficitur mauris sed, luctus nisl. Phasellus
-    aliquet nunc vitae vehicula aliquam. Aliquam condimentum risus sem, ut convallis nisi rutrum non. Praesent faucibus
-    erat augue, et pellentesque magna faucibus vitae. Proin sed maximus nisi. Cras sodales rutrum leo, sit amet interdum
-    justo consequat non. Donec id ultrices tortor, quis molestie ex. Cras eget ex at mauris posuere efficitur. Sed porta
-    ultrices varius.
-  </p>
-</template>
+<template />
+
+<script lang="ts">
+</script>
 
 <style>
 </style>
